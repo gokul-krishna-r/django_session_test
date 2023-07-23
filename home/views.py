3789d917b3c156ec19d008ecac9a2bf21c5262ca
Gokul <55547509+gokul-krishna-r@users.noreply.github.com>
2023-07-24 03:29:45 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(requesst):
-    context = {"text": "Start building"}
+    context = {"text": "First"}
     return render(requesst, "index.html", context=context)
